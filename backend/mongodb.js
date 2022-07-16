c93bec52a202e044c2599b28790968549eebe2c1
zeusVikalp <100758326+zeusVikalp@users.noreply.github.com>
2022-07-16 14:10:32 +0530

    done

--- backend/mongodb.js
+++ backend/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect('mongodb://localhost/project', () => {
+mongoose.connect('mongodb://localhost:27017', () => {
     console.log("mongodb is working")
 })
 
